9e31ac0234b37462515e1fb84436d6f2d42613bf
ahmeddmahmoud <ahmed.mahmoudd.elshahat@gmail.com>
2024-06-21 00:31:06 +0300

    creating Examlist Component

---
+++ exams/src/components/student/ExamList.js
@@ -0,0 +1,82 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchExams } from "../../store/slices/examSlice";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  Container,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Grid,
+} from "@mui/material";
+
+const ExamList = () => {
+  const dispatch = useDispatch();
+  const { exams } = useSelector((state) => state.exams);
+
+  useEffect(() => {
+    dispatch(fetchExams());
+  }, [dispatch]);
+
+  return (
+    <Container maxWidth="md" sx={{ mt: 5 }}>
+      <Typography
+        variant="h4"
+        component="h2"
+        align="center"
+        gutterBottom
+        sx={{
+          fontWeight: 600,
+          color: "#3949a0",
+          marginBottom: "1.5rem",
+        }}
+      >
+        Available Exams
+      </Typography>
+      <Grid container justifyContent="center">
+        <Grid item xs={12} sm={8}>
+          <Paper elevation={3} sx={{ p: 2 }}>
+              <List sx={{ width: "100%" }}>
+                {exams.map((exam) => (
+                  <ListItem
+                    key={exam._id}
+                    component={RouterLink}
+                    to={/exams/${exam._id}}
+                    sx={{
+                      borderRadius: 8,
+                      mb: 2,
+                      bgcolor: "#ffffff",
+                      transition: "background-color 0.3s",
+                      "&:hover": {
+                        bgcolor: "#f5f5f5",
+                      },
+                    }}
+                  >
+                    <ListItemText
+                      primary={
+                        <Typography
+                          variant="h6"
+                          sx={{ color: "#3949a0", fontWeight: 600 }}
+                        >
+                          {exam.title}
+                        </Typography>
+                      }
+                      secondary={
+                        <Typography variant="body1" sx={{ color: "#555" }}>
+                          {exam.description}
+                        </Typography>
+                      }
+                    />
+                  </ListItem>
+                ))}
+              </List>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default ExamList;
