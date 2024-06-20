ae0796af7d978192d52233546455d1dd328f28ca
Medhat Elbesy <152287116+MedhatElbesy@users.noreply.github.com>
2024-06-20 23:23:50 +0300

    handel route

--- exams/src/App.js
+++ exams/src/App.js
@@ -1,19 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import store from "./store/store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import AdminDashboard from "./components/admin/AdminDashboard";
+import ExamCreate from "./components/admin/ExamCreate";
 
 function App() {
   return (
+    <Provider store={store}>
+
       <Router>
     <Routes>
     <Route path="/admin" element={<AdminDashboard />} />
+    <Route path="/admin/create-exam" element={<ExamCreate />} />
 
       </Routes>
 
       </Router>
+    </Provider>
 
   );
 }
