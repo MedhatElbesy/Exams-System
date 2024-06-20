dd00f5fb9c920f24f1d35619642ca5b34d8a7ed3
Khaled Abdelbaset <Khaledabdelbaset98@gmail.com>
2024-06-20 23:42:26 +0300

    adding login route

--- exams/src/App.js
+++ exams/src/App.js
@@ -4,9 +4,14 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+import Login from "./components/auth/Login";
+
+
 import AdminDashboard from "./components/admin/AdminDashboard";
 import ExamCreate from "./components/admin/ExamCreate";
 import QuestionCreate from "./components/admin/QuestionCreate";
+
 
 
 function App() {
@@ -14,12 +19,13 @@ function App() {
     <Provider store={store}>
       <Router>
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/admin/create-exam" element={<ExamCreate />} />
           <Route
-              path="/admin/:examId/add-question"
-              element={<QuestionCreate />}
-            />
+            path="/admin/:examId/add-question"
+            element={<QuestionCreate />}
+          />
         </Routes>
       </Router>
       <ToastContainer position="top-center" autoClose={1500} />
