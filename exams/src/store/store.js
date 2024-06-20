633d4f49f015de39e699005795cd5f980b95797d
ahmeddmahmoud <ahmed.mahmoudd.elshahat@gmail.com>
2024-06-21 01:06:10 +0300

    adding result reducer in store

--- exams/src/store/store.js
+++ exams/src/store/store.js
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import examReducer from "./slices/examSlice";
 import authReducer from "./slices/authSlice";
-
+import resultsReducer from "./slices/resultSlice";
 
 // Configure Redux store
 const store = configureStore({
   reducer: {
     exams: examReducer,
     auth: authReducer,
+    results: resultsReducer,
   },
 });
 
