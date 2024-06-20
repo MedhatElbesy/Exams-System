e3663ac6d7647cf1c339773519b2c86c8c936821
Khaled Abdelbaset <Khaledabdelbaset98@gmail.com>
2024-06-20 23:47:12 +0300

    updating store

--- exams/src/store/store.js
+++ exams/src/store/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import examReducer from "./slices/examSlice";
+import authReducer from "./slices/authSlice";
 
 
 // Configure Redux store
 const store = configureStore({
   reducer: {
     exams: examReducer,
-
+    auth: authReducer,
   },
 });
 
