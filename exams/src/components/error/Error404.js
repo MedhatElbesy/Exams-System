11241c8f7b4e262c6c0bdd30e5e818d7ac854df5
Khaled Abdelbaset <Khaledabdelbaset98@gmail.com>
2024-06-21 23:19:34 +0300

    creating errors pages

---
+++ exams/src/components/error/Error404.js
@@ -0,0 +1,37 @@
+import { Container, Typography, Box } from "@mui/material";
+
+function NotFound() {
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          textAlign: "center",
+          color: "gray",
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{ fontWeight: "bold" }}
+        >
+          404
+        </Typography>
+        <Typography variant="h5" sx={{ marginTop: "1rem" }}>
+          Page Not Found
+        </Typography>
+        <Typography variant="body1" sx={{ marginTop: "0.5rem" }}>
+          The page you are looking for is unavailable.
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
+
+export default NotFound;
