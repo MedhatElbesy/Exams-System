eb1a1d142911491638e963e14a9cfc7711d9a9e0
Medhat Elbesy <152287116+MedhatElbesy@users.noreply.github.com>
2024-06-21 23:22:43 +0300

    routes

---
+++ exams/src/utils/ProtectedRoute.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoute = ({ admin }) => {
+  const isLoggedIn = !!sessionStorage.getItem("token");
+  const isAdmin = !!sessionStorage.getItem("isAdmin");
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (admin && !isAdmin) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
+};
+
+
+export { ProtectedRoute };
